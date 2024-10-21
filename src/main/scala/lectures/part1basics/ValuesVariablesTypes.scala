485d43a785d73dc1355a8e2f41a48e227a35f90d
mbergman <mbergmanpga@gmail.com>
2024-10-20 21:13:10 -0400

    adding initial values for first lessons

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,27 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  // vals are immutable
+
+  val x: Int = 42
+  println(x)
+
+
+  val aString : String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort : Short = 4613 // 2 bytes
+  val aLong: Long = 542465454
+
+  var aVariable: Int = 36
+
+  aVariable = x
+
+  println(aVariable)
+
+
+}
+
